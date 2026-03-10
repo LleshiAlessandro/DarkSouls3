22727924c34bb5084e355c590ea66832ab0d2dd6
lleshi_alessandro <lleshi.alessandro@franchettisalviani.net>
2026-03-10 13:25:22 +0100

    iniziato il fight manager
    ho implementato la prima idea del metodo fight

--- DarkSouls3/src/darksouls3/FightManager.java
+++ DarkSouls3/src/darksouls3/FightManager.java
@@ -9,5 +9,32 @@ package darksouls3;
  * @author aless
  */
 public class FightManager {
+    private int turni;
+
+    public FightManager() {
+        this.turni = 1;
+    }
     
+    public void incrementTurn(){
+        turni = turni + 1;
+    }
+    
+    //controlla di chi è il turno, nel gestore dovrò gestire cosa succede nel turno del giocatore e del boss
+    public boolean fight(Character c, Villain v){
+        if(c.getLife() > 0 && v.getLife() > 0){
+            
+            //turni del character
+            if(turni %2 != 0){
+                this.incrementTurn();
+                return true;
+            }
+            //turni del nemico
+            else{
+                //c.setLife(c.life = c.life - 33);
+                this.incrementTurn();
+                return false;
+            }
+        }
+        return false;
+    }
 }
